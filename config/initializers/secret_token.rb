561df89bab178712dd762abcc3b0eda836c43e9e
Paweł Okoń <pawel.witold.okon@gmail.com>
2014-02-23 22:26:48 +0100

    The journey beggins

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StockOOV2::Application.config.secret_key_base = 'ff2e041064da0bd4ad1b4da25e5491092614e320de87f3eba72fbd426ea4afda2166ba38fb9cdbccb6ae61d282284081a16a3512af7b18261ead91f7cd09cafb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StockOOV2::Application.config.session_store :cookie_store, key: '_stockOO_v2_session'
