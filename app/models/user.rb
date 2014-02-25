e8311c6e2e73d00f28b39647c998aa030c0be6fa
Paweł Okoń <pawel.witold.okon@gmail.com>
2014-02-25 22:49:04 +0100

    Setup of devise

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  before_save :ensure_authentication_token
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :token_authenticatable
+
+  def skip_confirmation!
+  self.confirmed_at = Time.now
+end
+end
