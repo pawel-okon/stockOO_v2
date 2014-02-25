f5842edc4e64b53a524e9708f8ab26b59fe64cf4
Paweł Okoń <pawel.witold.okon@gmail.com>
2014-02-25 20:29:24 +0100

    Changed default files generator from erb to haml

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module StockOOV2
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.template_engine :haml
+    end
   end
 end
