ee4547e2aefd00222475dfaff0d8c3c2e1e449ff
Paweł Okoń <p.okon@a2s.pl>
2014-03-22 14:27:26 +0100

    Added attributes for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+attr_accessible :name, :email, :password, :password_confirmation
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
