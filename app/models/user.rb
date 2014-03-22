6d6a6ec6ed03e2c66af7255fecba150c5159a51a
Paweł Okoń <p.okon@a2s.pl>
2014-03-22 14:25:47 +0100

    Added :confirmable to devise available modules and bypass confirmation step

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :validatable, :confirmable, :token_authenticatable
+
+  def skip_confirmation!
+    self.confirmed_at = Time.now
+  end
 end
