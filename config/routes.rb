ab9df986b45fab539f2e4d01d2f57dbee1f23467
Paweł Okoń <pawel.witold.okon@gmail.com>
2014-03-24 22:30:07 +0100

    Added register route to api namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ StockOOV2::Application.routes.draw do
   namespace :api do
   namespace :v1 do
     devise_scope :user do
+      post 'registrations' => 'registrations#create', :as => 'register'
       post 'sessions' => 'sessions#create', :as => 'login'
       delete 'sessions' => 'sessions#destroy', :as => 'logout'
     end
