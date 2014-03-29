35500f7fba1840401aaf044946cc16399998fcc8
Paweł Okoń <pawel.witold.okon@gmail.com>
2014-03-29 18:16:20 +0100

    Added sidebar option for Categories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def main_menu
       { :url => root_path, :label => t("menu_labels.home") }
   end
+
+  def sidebar_options(view_name)
+    case view_name
+    when :categories_index
+      [{ :url => new_category_path, :icon => "icon-plus", :label => t("views.categories_index.add_category") }]
+  end
+end
+
 end
