075a022a262a9228e5553bc0ff1b2006221ed8e2
Paweł Okoń <pawel.witold.okon@gmail.com>
2014-03-29 18:34:04 +0100

    Categories recentry added on home list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @categories = Category.where("created_at >= :start_date", :start_date => Time.now.midnight).order("created_at DESC")
   end
 end
