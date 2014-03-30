38f159473edf4705afd942308aa5c2e01784f168
Paweł Okoń <pawel.witold.okon@gmail.com>
2014-03-30 08:08:01 +0200

    Setting up translations

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module StockOOV2
   class Application < Rails::Application
-    config.i18n.enforce_available_locales = true
+    # config.i18n.enforce_available_locales = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -16,7 +16,7 @@ module StockOOV2
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Warsaw'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
