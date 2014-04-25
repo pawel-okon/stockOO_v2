f26b2d36b1bcf4f5f3763a01fb0c3844b1323ff8
Paweł Okoń <pawel.witold.okon@gmail.com>
2014-04-25 04:31:01 +0200

    Skiping validation of locale

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module StockOOV2
   class Application < Rails::Application
-    # config.i18n.enforce_available_locales = true
+    config.i18n.enforce_available_locales = false 
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
