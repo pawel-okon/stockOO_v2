a6c1f0074f74f9af91d68cb4584ec02c23d2cfbc
Paweł Okoń <pawel.witold.okon@gmail.com>
2014-06-02 09:11:42 +0200

    Updated Products cosmetics in display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @categories = Category.where("created_at >= :start_date", :start_date => Time.now.midnight).order("created_at DESC")
+    products = Product.where("created_at >= :start_date", :start_date => Time.now.midnight).order("created_at DESC")
   end
 end
