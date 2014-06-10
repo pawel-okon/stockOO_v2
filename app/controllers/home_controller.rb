113095fa4f4d30ca3a902e289923f9524bdb958b
Paweł Okoń <pawel.witold.okon@gmail.com>
2014-06-10 21:03:49 +0200

    fixed fuckup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    products = Product.where("created_at >= :start_date", :start_date => Time.now.midnight).order("created_at DESC")
+    @products = Product.where("created_at >= ?", Time.now.midnight)
+      .order("created_at DESC")
   end
 end
