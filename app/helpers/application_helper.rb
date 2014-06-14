68a2e3d1cfde845846ff9003bfbcf7a98aec8e13
Paweł Okoń <pawel.witold.okon@gmail.com>
2014-06-14 23:23:16 +0200

    Changed line lenght

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,8 @@ module ApplicationHelper
       { :url => root_path, :label => t("views.home.menu_label.main") },
       { :url => api_v1_categories_path,
         label: t("views.home.menu_label.category") },
-      { :url => api_v1_manufacturers_path, :label => t("views.home.menu_label.manufacturer") },
+      { :url => api_v1_manufacturers_path,
+        :label => t("views.home.menu_label.manufacturer") },
       { :url => api_v1_products_path, :label => 'Produkty' }
     ]
   end
@@ -12,11 +13,16 @@ module ApplicationHelper
   def sidebar_options(view_name)
     case view_name
     when :categories_index
-      [{ :url => new_api_v1_category_path, :icon => "icon-plus-sign", :label => t("views.categories.create") }]
+      [{ :url => new_api_v1_category_path, :icon => "icon-plus-sign",
+         :label => t("views.categories.create") }]
     when :manufacturers_index
-      [{ :url => new_api_v1_manufacturer_path, :icon => "icon-plus-sign", :label => t("views.manufacturers.create") }]
+      [{ :url => new_api_v1_manufacturer_path, :icon => "icon-plus-sign",
+         :label => t("views.manufacturers.create") }]
     when :products_index
-      [{ :url => new_api_v1_product_path, :icon => "icon-plus-sign", :label => t("views.products.create") }]
+      [{ :url => new_api_v1_product_path, :icon => "icon-plus-sign",
+         :label => t("views.products.create") }]
     end
   end
+
+
 end
