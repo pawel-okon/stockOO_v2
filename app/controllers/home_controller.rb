bb8e5cad001eb6c46cffdc8724982135c5a5bc58
Paweł Okoń <pawel.witold.okon@gmail.com>
2014-06-21 21:08:19 +0200

    Added manufacturer and category last added into dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@ class HomeController < ApplicationController
   def index
     @products = Product.where("created_at >= ?", Time.now.midnight)
       .order("created_at DESC")
+    @categories = Category.where("created_at >= ?", Time.now.midnight)
+      .order("created_at DESC")
+    @manufacturers = Manufacturer.where("created_at >= ?", Time.now.midnight)
+      .order("created_at DESC")
   end
 end
